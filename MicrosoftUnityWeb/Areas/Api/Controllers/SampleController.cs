8b4f8b662889f842ba4277e6ec13d7505dec7fcb
Asadkc <asadullahchoudhry@yahoo.com>
2017-07-01 14:56:56 -0700

    First Checkin to repository.  Added Sample Controller with bugs.

---
+++ MicrosoftUnityWeb/Areas/Api/Controllers/SampleController.cs
@@ -0,0 +1,33 @@
+﻿using DataAccess;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MicrosoftUnityWeb.Areas.Api.Controllers
+{
+    public class SampleController : ApiController
+    {
+        private IFacade _facade;
+        public Facade FacadeInstance { get; set; }
+
+        public SampleController()
+        {
+
+        }
+        public SampleController(IFacade facade)
+        {
+            this._facade = facade;
+        }
+
+        public IQueryable <Sample> Get()
+        {
+            return FacadeInstance.GetAllSamples().ToList().AsQueryable();
+        }
+
+
+    }
+}
